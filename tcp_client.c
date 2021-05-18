b7a89a2c548ae1c49a2af4b4b75ce4a878b754bc
dangquanghuy107 <dangquanghuy107@gmail.com>
2021-05-18 15:39:36 +0700

    update codes

---
+++ tcp_client.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+int main() {
+    int network_socket; 
+
+    // create a socket
+    network_socket = socket(AF_INET, SOCK_STREAM, 0);  
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET; 
+    server_address.sin_port = htons(8001);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address)); // return 0 if success, -1 otherwise
+
+    if (connection_status == -1){ 
+        printf("Error: Cannot connect %d", connection_status);
+        return -1;
+    }
+
+    char server_response[256];
+    recv(network_socket, &server_response, sizeof(server_response), 0);
+
+    printf("Response %s\n", server_response);
+    close(network_socket);
+
+    return 0;
+}
